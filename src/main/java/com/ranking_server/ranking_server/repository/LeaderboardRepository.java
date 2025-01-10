60f973dcc5ad3ca46f3ce3682945505e616e4307
tasoo0924@gmail.com <tasoo0924@naver.com>
2025-01-11 01:55:42 +0900

    commit

--- src/main/java/com/ranking_server/ranking_server/repository/LeaderboardRepository.java
+++ src/main/java/com/ranking_server/ranking_server/repository/LeaderboardRepository.java
@@ -21,6 +21,5 @@ public interface LeaderboardRepository extends JpaRepository<ScoreEntry, Long> {
     
     @Query("SELECT se.playerName FROM ScoreEntry se WHERE se.guid=:guid")
     String getName( @Param("guid") String guid);
-          
-
+         
 }
